8a53e29979f2f474b3ff34d8fc1ff657f0e84463
petrukmax <4998198+petrukmax@users.noreply.github.com>
2025-02-09 11:38:40 +0300

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Запускаем Python-скрипт с параметрами
-C:\Users\petrukmax\AppData\Local\Programs\Python\Python37\python.exe sort.py --folder ..\ --labels nero ero cosplay erocos portret DELETE
+C:\Users\petrukmax\AppData\Local\Programs\Python\Python37\python.exe sort.py --folder ..\ --labels cat dog other
 pause
